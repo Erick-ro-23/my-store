9543105bf7c6c88a89de30cd380775aa5112bc12
Erick-ro-23 <76449897+Erick-ro-23@users.noreply.github.com>
2023-02-08 16:52:23 +0100

    Creamos el detalle de cada producto con slides, un servicio nuevo de getOne y boton para volver a la página anterior

---
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
+import { Producto } from '../../models/product.model';
+import { ProductsService } from '../../services/products.service';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss'],
+})
+export class ProductDetailComponent implements OnInit {
+  productId: string | null = null;
+  product: Producto | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap
+      .pipe(
+        switchMap((params) => {
+          this.productId = params.get('id');
+          if (this.productId) {
+            return this.productsService.getOne(this.productId);
+          }
+          return [null];
+        })
+      )
+      .subscribe((data) => {
+        this.product = data;
+      });
+  }
+
+  goToBack() {
+    this.location.back();
+  }
+}
